079aec1872a387ced7920345abc6f5413ac0f544
Maiyarasan <maiyarasn11@gmail.com>
2024-03-25 11:58:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [todo, settodo] = useState([]);;
+  const [inputValue, setInputValue] = useState('')
+  
+  const add=()=>{
+    settodo([...todo , inputValue]);
+    setInputValue('');
+   
+  }
+  const del=(index)=>{
+    const  newTodo=[...todo]
+    newTodo.splice(index,1);
+    settodo(newTodo)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1>Maiyarasan's Todo App</h1>
+    <div className='container'>
+      <div>
+        <input 
+          type='text'
+          value={inputValue}
+          onChange={(e)=>setInputValue(e.target.value)}
+        />
+        <button onClick={add}>Add</button>
+        <ul>
+          {todo.map((addon , index)=>(
+             <li key={index}>
+              {addon} 
+              <button onClick={del}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
